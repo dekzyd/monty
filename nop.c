4aa4911864d2ec296400ef0c9c440b01da8bb3fa
dekzyd <adekoyadayo2010@gmail.com>
2022-12-14 14:04:35 -0800

    Update push

--- nop.c
+++ nop.c
@@ -1,27 +1,15 @@
 #include "monty.h"
 
-
-
 /**
-
  * _nop - doesn’t do anything.
-
  * @stack: head
-
  * @num_line: number of the line
-
  */
 
 void _nop(stack_t **stack, unsigned int num_line)
-
 {
-
 	if (*stack)
-
 	{};
-
 	if (num_line)
-
 	{};
-
 }
--- pall.c
+++ pall.c
@@ -1,39 +1,21 @@
 #include "monty.h"
 
-
-
 /**
-
  * _pall - prints all the values on the stack,
-
  * starting from the top of the stack
-
  * @stack: head
-
  * @num_line: num line
-
  */
 
 void _pall(stack_t **stack, unsigned int num_line)
-
 {
-
 	stack_t *temp;
 
-
-
 	temp = *stack;
-
 	if (num_line)
-
 		while (temp)
-
 		{
-
 			printf("%d\n", temp->n);
-
 			temp = temp->next;
-
 		}
-
 }
