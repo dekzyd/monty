79eaf9ad5dac3e9a613b953eb67c153fbc50b643
dekzyd <adekoyadayo2010@gmail.com>
2022-12-14 13:27:03 -0800

    Initial commit

---
+++ pall.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+
+
+
+/**
+
+ * _pall - prints all the values on the stack,
+
+ * starting from the top of the stack
+
+ * @stack: head
+
+ * @num_line: num line
+
+ */
+
+void _pall(stack_t **stack, unsigned int num_line)
+
+{
+
+	stack_t *temp;
+
+
+
+	temp = *stack;
+
+	if (num_line)
+
+		while (temp)
+
+		{
+
+			printf("%d\n", temp->n);
+
+			temp = temp->next;
+
+		}
+
+}
